ca21a6b187cc00dedcd22fa0f71ebeef6e92c031
romanOVF <43718503+romanOVF@users.noreply.github.com>
2018-10-02 18:17:52 +0500

    Delete RunProgram.java

--- RunProgram.java
+++
@@ -1,9 +0,0 @@
-// Created oct 01 mon 2018
-// reset && javac -d bin RunProgram.java && java -cp bin RunProgram
-
-public class RunProgram {
-  public static void main ( String [] args ) {
-    Downloader down = new Downloader ();
-    down.start ();
-  }
-}
